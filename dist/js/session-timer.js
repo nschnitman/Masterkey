db224617f26a7aa3fc49c4380ce8823056353761
Nicolas Schnitman <nicosch95@gmail.com>
2022-04-03 17:53:33 +0300

    logo update

--- dist/js/session-timer.js
+++ dist/js/session-timer.js
@@ -1,4 +1,4 @@
-var hours = 0.1; // to clear the localStorage after 1 hour(if someone want to clear after 8hrs simply change hours=8)
+var hours = 0.1; 
 var now = new Date().getTime();
 var setupTime = localStorage.getItem('setupTime');
 if (setupTime == null) {

f977a11d9a83446014034d11e18121dbbaa177ba
Nicolas Schnitman <nicosch95@gmail.com>
2022-04-05 21:04:19 +0000

    extra update

--- upload.php
+++ upload.php
@@ -1,16 +1,75 @@
 <?php
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-/* Get the name of the uploaded file */
-$filename = $_FILES['file']['name'];
-$tz = $_POST['tz']
-/* Choose where to save the uploaded file */
-$location = "upload/".$filename;
-
-/* Save the uploaded file to the local filesystem */
-if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
-  echo 'Success'; 
-} else { 
-  echo 'Failure'; 
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+	echo "File is an image - " . $check["mime"] . ".";
+	$uploadOk = 1;
+  } else {
+	echo "File is not an image.";
+	$uploadOk = 0;
+  }
 }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+$servername = "179.43.123.27";
+$username = "pj-masterkey";
+$password = "Monday29/Lost";
+$dbname = "pj-masterkey";
+
+$tz = $_POST["tz"];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = 'INSERT INTO extras (tz, photo_tz) VALUES ('.$tz.',"'.$target_file.'" )';
+
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	echo "The file has been uploaded.";
+	
+	if ($conn->query($sql) === TRUE) {
+	  echo "New record created successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+  } else {
+	echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+$conn->close();
 
 ?>

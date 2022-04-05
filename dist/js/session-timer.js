e13da3b00d6bfb0b1b0aa2fabbda55498040f3bd
Nicolas Schnitman <nicosch95@gmail.com>
2022-04-05 14:08:23 +0000

    timer update

--- dist/js/session-timer.js
+++ dist/js/session-timer.js
@@ -1,13 +1,13 @@
-var hours = 0.5; 
-var now = new Date().getTime();
-var setupTime = localStorage.getItem('setupTime');
-if (setupTime == null) {
-    localStorage.setItem('setupTime', now)
-} else {
-    if(now-setupTime > hours*60*60*1000) {
-        localStorage.clear()
-        localStorage.setItem('setupTime', now);
-        top.location.href = "login.php"
-        
-    }
-}
+// var hours = 2; 
+// var now = new Date().getTime();
+// var setupTime = localStorage.getItem('setupTime');
+// if (setupTime == null) {
+//     localStorage.setItem('setupTime', now)
+// } else {
+//     if(now-setupTime > hours*60*60*1000) {
+//         localStorage.clear()
+//         localStorage.setItem('setupTime', now);
+//         top.location.href = "login.php"
+//         
+//     }
+// }

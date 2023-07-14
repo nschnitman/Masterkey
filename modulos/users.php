909e694eb111db38af8045a5f7f97796536971b1
Nico <nicosch95@gmail.com>
2023-07-14 23:47:11 +0300

    Users Update

--- modulos/users.php
+++ modulos/users.php
@@ -1,13 +1,27 @@
-<? include("../header.php") ?>
-<base href="../" />
-<title>Pj-MasterKey - עובדים</title>
-<!-- Custom CSS -->
-<link rel="stylesheet" type="text/css" href="assets/extra-libs/multicheck/multicheck.css">
-<link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
-<link href="dist/css/style-rtl.min.css" rel="stylesheet">
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
+<!DOCTYPE html>
+<html dir="rtl" lang="he">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Web App for exchange of master keycard for employes">
+	<meta name="author" content="Nicolas Schnitman">
+	<!-- Favicon icon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="assets/favicon/site.webmanifest">
+	<base href="../" />
+	<title>Pj-MasterKey - עובדים</title>
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/extra-libs/multicheck/multicheck.css">
+	<link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
+	<link href="dist/css/style-rtl.min.css" rel="stylesheet">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
@@ -151,143 +165,143 @@
 		<!-- ============================================================== -->
 		<!-- End Left Sidebar - style you can find in sidebar.scss  -->
 		<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Page wrapper  -->
-<!-- ============================================================== -->
-<div class="page-wrapper">
-    <!-- ============================================================== -->
-    <!-- Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
-    <div class="page-breadcrumb">
-        <div class="row">
-            <div class="col-12 d-flex no-block align-items-center">
-                <h4 class="page-title">עודבי מלון</h4>
-                <div class="ml-auto text-right">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">בית</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">עובדים</li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Container fluid  -->
-    <!-- ============================================================== -->
-    <div class="container-fluid">
-        <!-- ============================================================== -->
-        <!-- Start Page Content -->
-        <!-- ============================================================== -->
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">רשימה עובדי מלון</h5>
-                        <div class="table-responsive">
-                            <table id="zero_config" class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>מס׳</th>
-                                        <th>שם</th>
-                                        <th>שם משפחה</th>
-                                        <th>טלפון</th>
-                                        <th>תפקיד</th>
-                                        <th>מחלקה</th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
+		<!-- ============================================================== -->
+		<!-- Page wrapper  -->
+		<!-- ============================================================== -->
+		<div class="page-wrapper">
+			<!-- ============================================================== -->
+			<!-- Bread crumb and right sidebar toggle -->
+			<!-- ============================================================== -->
+			<div class="page-breadcrumb">
+				<div class="row">
+					<div class="col-12 d-flex no-block align-items-center">
+						<h4 class="page-title">עודבי מלון</h4>
+						<div class="ml-auto text-right">
+							<nav aria-label="breadcrumb">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="#">בית</a></li>
+									<li class="breadcrumb-item active" aria-current="page">עובדים</li>
+								</ol>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- ============================================================== -->
+			<!-- End Bread crumb and right sidebar toggle -->
+			<!-- ============================================================== -->
+			<!-- ============================================================== -->
+			<!-- Container fluid  -->
+			<!-- ============================================================== -->
+			<div class="container-fluid">
+				<!-- ============================================================== -->
+				<!-- Start Page Content -->
+				<!-- ============================================================== -->
+				<div class="row">
+					<div class="col-12">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title">רשימה עובדי מלון</h5>
+								<div class="table-responsive">
+									<table id="zero_config" class="table table-striped table-bordered">
+										<thead>
+											<tr>
+												<th>מס׳</th>
+												<th>שם</th>
+												<th>שם משפחה</th>
+												<th>טלפון</th>
+												<th>תפקיד</th>
+												<th>מחלקה</th>
+											</tr>
+										</thead>
+									</table>
+								</div>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- ============================================================== -->
-        <!-- End PAge Content -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right sidebar -->
-        <!-- ============================================================== -->
-        <!-- .right-sidebar -->
-        <!-- ============================================================== -->
-        <!-- End Right sidebar -->
-        <!-- ============================================================== -->
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Container fluid  -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- footer -->
-    <!-- ============================================================== -->
-    <footer class="footer text-center" dir="ltr">
-        All Rights Reserved. Designed and Developed by <a href="https://nicolasschnitman.com">Nicolas Schnitman</a>.
-    </footer>
-    <!-- ============================================================== -->
-    <!-- End footer -->
-    <!-- ============================================================== -->
-</div>
-<!-- ============================================================== -->
-<!-- End Page wrapper  -->
-<!-- ============================================================== -->
-</div>
-<!-- ============================================================== -->
-<!-- End Wrapper -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- All Jquery -->
-<!-- ============================================================== -->
-<script src="assets/libs/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap tether Core JavaScript -->
-<script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-<script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- slimscrollbar scrollbar JavaScript -->
-<script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-<script src="assets/extra-libs/sparkline/sparkline.js"></script>
-<!--Wave Effects -->
-<script src="dist/js/waves.js"></script>
-<!--Menu sidebar -->
-<script src="dist/js/sidebarmenu.js"></script>
-<!--Custom JavaScript -->
-<script src="dist/js/custom.min.js"></script>
-<!-- this page js -->
-<script src="assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
-<script src="assets/extra-libs/multicheck/jquery.multicheck.js"></script>
-<script src="assets/extra-libs/DataTables/datatables.min.js"></script>
-<script>
-    /****************************************
-     *       Basic Table                   *
-     ****************************************/
-    $('#zero_config').DataTable({
-        ajax: {
-            url: 'https://pj-serverless.vercel.app/api/users/view',
-            dataSrc: ''
-        },
-        columns: [{
-                data: 'id'
-            },
-            {
-                data: 'name'
-            },
-            {
-                data: 'last_name'
-            },
-            {
-                data: 'telefono'
-            },
-            {
-                data: 'tafkid'
-            },
-            {
-                data: 'majlaka'
-            }
-        ]
-    });
-</script>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- ============================================================== -->
+				<!-- End PAge Content -->
+				<!-- ============================================================== -->
+				<!-- ============================================================== -->
+				<!-- Right sidebar -->
+				<!-- ============================================================== -->
+				<!-- .right-sidebar -->
+				<!-- ============================================================== -->
+				<!-- End Right sidebar -->
+				<!-- ============================================================== -->
+			</div>
+			<!-- ============================================================== -->
+			<!-- End Container fluid  -->
+			<!-- ============================================================== -->
+			<!-- ============================================================== -->
+			<!-- footer -->
+			<!-- ============================================================== -->
+			<footer class="footer text-center" dir="ltr">
+				All Rights Reserved. Designed and Developed by <a href="https://nicolasschnitman.com">Nicolas Schnitman</a>.
+			</footer>
+			<!-- ============================================================== -->
+			<!-- End footer -->
+			<!-- ============================================================== -->
+		</div>
+		<!-- ============================================================== -->
+		<!-- End Page wrapper  -->
+		<!-- ============================================================== -->
+	</div>
+	<!-- ============================================================== -->
+	<!-- End Wrapper -->
+	<!-- ============================================================== -->
+	<!-- ============================================================== -->
+	<!-- All Jquery -->
+	<!-- ============================================================== -->
+	<script src="assets/libs/jquery/dist/jquery.min.js"></script>
+	<!-- Bootstrap tether Core JavaScript -->
+	<script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+	<script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- slimscrollbar scrollbar JavaScript -->
+	<script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+	<script src="assets/extra-libs/sparkline/sparkline.js"></script>
+	<!--Wave Effects -->
+	<script src="dist/js/waves.js"></script>
+	<!--Menu sidebar -->
+	<script src="dist/js/sidebarmenu.js"></script>
+	<!--Custom JavaScript -->
+	<script src="dist/js/custom.min.js"></script>
+	<!-- this page js -->
+	<script src="assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
+	<script src="assets/extra-libs/multicheck/jquery.multicheck.js"></script>
+	<script src="assets/extra-libs/DataTables/datatables.min.js"></script>
+	<script>
+		/****************************************
+		 *       Basic Table                   *
+		 ****************************************/
+		$('#zero_config').DataTable({
+			ajax: {
+				url: 'https://pj-serverless.vercel.app/api/users/view',
+				dataSrc: ''
+			},
+			columns: [{
+					data: 'id'
+				},
+				{
+					data: 'name'
+				},
+				{
+					data: 'last_name'
+				},
+				{
+					data: 'telefono'
+				},
+				{
+					data: 'tafkid'
+				},
+				{
+					data: 'majlaka'
+				}
+			]
+		});
+	</script>
 
 </body>
 
